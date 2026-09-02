0e9110baca213d0f19ac3378b44e754ba093d426
Jeet Maheshwari <jeetmaheshwari1919@gmail.com>
2026-09-02 19:40:41 +0530

    Implement REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,21 @@
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Shell starting...");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            System.out.print("$: ");
+            String input = br.readLine();
+            if (input == null) {
+                break;
+            }
+            if (!input.isEmpty()) {
+                System.out.println("You entered: " + input);
+            }
+        }
     }
 }
